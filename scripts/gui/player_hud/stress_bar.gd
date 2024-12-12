4dcbada4ab9509b2cf02f41ff4555004d50bdd05
Yosh Nebe <sauteboy@gmail.com>
2024-12-12 11:22:21 -0800

    renamed and organized files

---
+++ scripts/gui/player_hud/stress_bar.gd
@@ -0,0 +1,17 @@
+class_name IntoxBar extends ProgressBar
+
+@export var player: Player
+
+
+func _ready() -> void:
+	var sb = StyleBoxFlat.new()
+	add_theme_stylebox_override("fill", sb)
+	sb.bg_color = Color("blue")
+	#player.intox_changed.connect(update_intox)
+	#update_intox()
+	#
+#func update_intox():
+	#value = player.current_intox * 100 / player.max_intox
+	#
+
+	
